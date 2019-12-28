dbeb99478e4a451145606043df11459c877aa346
Max Headroom <45197509+OnlyM1ss@users.noreply.github.com>
2019-12-28 16:38:15 +0200

    Add files via upload

--- scan.py
+++ scan.py
@@ -1,6 +1,10 @@
 import socket
 import re
+import time
+import threading
 
+def toFixed(numObj, digits=0):
+    return f"{numObj:.{digits}f}"
 def scan_port(ip,port):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.settimeout(0.5)
@@ -9,11 +13,18 @@ def scan_port(ip,port):
         print("Port:",port,"it's open")
         connect.close()
     except:
+        #print('не имеется открытых портов')
         pass
 a = input()
 b = input()
 c = input()
 d = input()
+
 ip = (str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d))
-for i in range(1000):
+start_time = time.time()
+for i in range(10000):
     scan_port(ip,i)
+    if (i == 0):
+        print('не имеется открытых портов')
+TimeOfProgram = (time.time() - start_time)
+print (toFixed((TimeOfProgram),2),'second')
