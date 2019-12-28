33bf0f131dca4d957c8eec43990361133a1ddcfe
Max Headroom <45197509+OnlyM1ss@users.noreply.github.com>
2019-12-28 17:25:30 +0200

    Add files via upload

--- scan.py
+++ scan.py
@@ -2,6 +2,7 @@ import socket
 import re
 import time
 import threading
+import progressbar
 
 def toFixed(numObj, digits=0):
     return f"{numObj:.{digits}f}"
@@ -13,17 +14,21 @@ def scan_port(ip,port):
         print("Port:",port,"it's open")
         connect.close()
     except:
-        #print('не имеется открытых портов')
         pass
 ip = input('введите ip:\n')
-#b = input()
-#c = input()
-#d = input()
-#ip = (str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d))
 start_time = time.time()
+bar = progressbar.ProgressBar(maxval=10.0).start()
+
+t = 0
+while t<= 1.0:
+    bar.update(t)
+    time.sleep(0.01)
+    t+=0.01
 for i in range(10000):
     scan_port(ip,i)
     if (i == 0):
         print('не имеется открытых портов')
+time.sleep(0.1)
+bar.finish()
 TimeOfProgram = (time.time() - start_time)
 print (toFixed((TimeOfProgram),2),'second')
